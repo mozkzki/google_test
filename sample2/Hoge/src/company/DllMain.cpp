ae2c8775190e548ee3e7781866955e43743fe627
mozkzki <kato.yutaka@gmail.com>
2021-09-16 06:00:03 +0900

    フォルダ構成変更

---
+++ sample2/Hoge/src/company/DllMain.cpp
@@ -0,0 +1,9 @@
+﻿/* Copyright 2017 mozkzki */
+#include <Windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hDLL, DWORD dwReason, LPVOID lpReserved) {
+    hDLL;
+    dwReason;
+    lpReserved;
+    return TRUE;
+}
