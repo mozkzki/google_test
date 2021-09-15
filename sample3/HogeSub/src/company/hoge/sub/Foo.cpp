164e10506c678c299d017e79ed475407ba08a423
mozkzki <kato.yutaka@gmail.com>
2021-09-16 06:02:26 +0900

    フォルダ構成変更

---
+++ sample3/HogeSub/src/company/hoge/sub/Foo.cpp
@@ -0,0 +1,14 @@
+﻿/* Copyright 2017 mozkzki */
+#include "company/hoge/sub/Foo.h"
+
+namespace company {
+namespace hoge {
+namespace sub {
+
+Foo::Foo() {
+    this->num_ = 0;
+}
+
+} // namespace sub
+} // namespace hoge
+} // namespace company
